dd5d22cda167c6dfd6e2c0a478b40992c7fd03ad
deviant-zairus <108suns@gmail.com>
2016-10-28 12:22:29 -0400

    Update playerFormat.cpp

--- playerFormat.cpp
+++ playerFormat.cpp
@@ -1,2 +1,19 @@
 #include "playerFormat.h"
 
+player::player(std::string name, std::string area, int level = 1, int Exp = 0)
+{
+  setName(name);
+  setArea(area);
+  setLevel(level);
+  setExp(Exp);
+  setExpRequired();
+  setMaxHealth();
+  setHealth(playerMaxHealth);
+  setStrength();
+  setConstitution();
+  setPerception();
+  setDexterity();
+  setIntelligence();
+  setWisdom();
+}
+
