bb29bd270eb34697e5da8b6a4d1ad4279f408f10
deviant-zairus <108suns@gmail.com>
2016-11-09 16:34:46 -0500

    Update game.h

--- game.h
+++ game.h
@@ -1,3 +1,29 @@
 #include "player.h"
 #include "mob.h"
 
+class Game
+{
+public:
+   void Run
+   {
+     Initialize();
+     while (_running)
+       Frame();
+     Shutdown();
+   }
+private:
+  void Initialize() 
+  {
+    
+  }
+  void Shutdown()
+  {
+    
+  }
+  void Frame() 
+  {
+  
+  }
+  bool _running = true;
+    
+    
