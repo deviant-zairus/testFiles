d21d0dc0798aa32a44e6fd6520ad93449833e297
deviant-zairus <108suns@gmail.com>
2016-11-17 10:15:28 -0500

    Create mob.cpp

---
+++ creatures/mob.cpp
@@ -0,0 +1,23 @@
+#inlude "mob.h"
+#include <iostream>
+
+mob::mob(std::string name, std::string area, std::string special, int level, int expGiven, int health, int maxHealth, int strength, int constitution, int intelligence, int wisdom, int dexterity, int perception)
+{
+  std::string setName(name);
+  std::string setArea(area);
+  std::string setSpecial(special);
+  int setLevel(level);
+  int setExpGiven(expGiven);
+  int setMaxHelth(maxHealth);
+  int setHealth(maxHealth);
+  int setStrength(strength);
+  int setConstitution(constitution);
+  int setPerception(perception);
+  int setDexterity(dexterity);
+  int setIntelligence(intelligence);
+  int setWisdom(wisdom);
+}
+
+//member mutators
+
+
